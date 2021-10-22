04b5771956e61ac980a605fb3ab002842214cb15
AletteXin <alettengxinling@gmail.com>
2021-10-22 10:07:57 +0800

    edits

--- healthrecord_api/blueprints/records/views.py
+++ healthrecord_api/blueprints/records/views.py
@@ -6,6 +6,9 @@ from flask import Blueprint, jsonify, request
 from playhouse.shortcuts import model_to_dict, dict_to_model
 import json
 import datetime as dt
+import peewee as pw
+import datetime
+from database import db
 
 
 records_api_blueprint = Blueprint('records_api',
@@ -19,17 +22,30 @@ def index():
 
     return jsonify(all_records)
 
-@records_api_blueprint.route("/create")
+@records_api_blueprint.route("/create", methods = ["POST"])
 def create():
-    Form.create(Name = "Amy", Gender = "female", Birthdate = dt.date(1940, 1, 21), 
-    Address = "Klang Valley", Medications = "high blood pressure pills", Nameofnextkin = "Mary",
-    Phoneofnextkin = "0123456789", Reasonforvisit = "To complete a check up", Fever = False, 
+    
+    print("connected")
+    new_entry = request.get_json()
+    print('new_entry')
+    print(new_entry)
+
+    current_time = datetime.datetime.now
+    now = print(current_time)
+
+    create = Form.create(Name = new_entry['Name'], Gender = new_entry['Gender'], Birthdate = new_entry['Birthdate'], 
+    Address = new_entry['Address'], Medications = new_entry['Medications'], Nameofnextkin = new_entry['Nameofnextkin'],
+    Phoneofnextkin = new_entry['Phoneofnextkin'], Reasonforvisit = new_entry['Reasonforvisit'], Fever = False, 
     Headache = True, Nightchills = False, Sorethroat = False, Cough = False, Breathingdiff = False, 
     Diarrhoea = True, Chestpain = False, Legnumbness = False, Handnumbness = False, Facenumbness = False, 
     Diabetes = False, Highbloodpressure = True, Highcholesterol = False, Asthma = False, Kidneydisease = False,
     Arthritis = False, Pancreaticcancer = False, Livercancer = False, Colorectalcancer = False, 
     COPD = True, Depression = False, Lungcancer = False)
-    return "create"
+
+    message = []
+    message.append(create.id)
+    
+    return jsonify({'message' : message})
 
 @records_api_blueprint.route("/update")
 def update():
@@ -52,26 +68,24 @@ def show():
     print(user_input)
 
     record_exists = Form.get_or_none(Form.id == user_input['id'], Form.Name == user_input['Name'])
-
+    
+    record_details = []
+    
     if record_exists:
-        id = record_exists.id
-        Name = record_exists.Name
-        Gender = record_exists.Gender
-        Birthdate = record_exists.Birthdate
-        Address = record_exists.Address
-        Medications = record_exists.Medications
-        Nameofnextkin = record_exists.Nameofnextkin
-        Phoneofnextkin = record_exists.Phoneofnextkin
-        Reasonforvisit = record_exists.Reasonforvisit
-
-        record_details = []
-        
         record_details.append([{'id':record_exists.id, 'Name': record_exists.Name, 
         'Gender': record_exists.Gender, 'Birthdate': record_exists.Birthdate, 
         'Address': record_exists.Address, 'Medications': record_exists.Medications, 
         'Nameofnextkin': record_exists.Nameofnextkin, 'Phoneofnextkin': record_exists.Phoneofnextkin,
-        'Reasonforvisit': record_exists.Reasonforvisit }])
+        'Reasonforvisit': record_exists.Reasonforvisit, 'Errormessage': "" }])
 
-        print(record_details)
+    
+    else: 
+        record_details.append([{'id':"", 'Name': "", 
+        'Gender': "", 'Birthdate': "", 
+        'Address': "", 'Medications': "", 
+        'Nameofnextkin': "", 'Phoneofnextkin': "",
+        'Reasonforvisit': "", 'Errormessage': "Record does not exist." }])
+
+    print(record_details)
 
     return jsonify({'record_details' : record_details})
